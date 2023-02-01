cb75585722a3a06207bc1d4033b41d7656813f48
Francesco Corsello <francesco.corsello@gmail.com>
2023-02-01 11:25:10 +0100

    initial commit

---
+++ props_05/src/Age.js
@@ -0,0 +1,10 @@
+// Extract the "Your age is " message into a new component called Age and render it within the Welcome component. 
+// Pass to the Age component the age prop that Welcome is receiving from the App component.
+
+import React from "react";
+
+export class Age extends React.Component{
+    render(){
+        return "Your age is"  
+    }
+}
