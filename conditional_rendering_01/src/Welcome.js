ef37b1958979cf79d9c140d01de827cbac6f6850
Francesco Corsello <francesco.corsello@gmail.com>
2023-02-03 19:48:18 +0100

    initial commit

---
+++ conditional_rendering_01/src/Welcome.js
@@ -0,0 +1,23 @@
+// Modify the Welcome component so that the Age component 
+// is rendered only if the age prop is greater than 18.
+
+import React from "react";
+import { Age } from "./Age";
+
+export class Welcome extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Welcome, {this.props.name}!</h1>
+                {this.props.age >= 18
+                ? <p> <Age /> {this.props.age}</p>
+                : <p></p>
+                }
+            </div>
+        ) 
+    }
+}
+Welcome.defaultProps = {
+    name: "Pippo",
+    age: 18
+} 
