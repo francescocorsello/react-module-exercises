67c846e7474142ee72b87d1f40d93800e35627b7
Francesco Corsello <44068027+francescocorsello@users.noreply.github.com>
2023-02-07 19:30:31 +0100

    Delete component_lifecycle_02 directory

--- component_lifecycle_02/src/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-import { Counter } from './Counter';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Counter  start={10} amount={10} interval={200} />)
