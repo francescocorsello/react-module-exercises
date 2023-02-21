0d97ccb53adfea26d0614c6d466d97668bca57a2
Francesco Corsello <44068027+francescocorsello@users.noreply.github.com>
2023-02-21 11:42:12 +0100

    Update Container.js

--- component_composition_01/src/Container.js
+++ component_composition_01/src/Container.js
@@ -8,7 +8,6 @@ export class Container extends React.Component{
     render(){
         return(
             <div className="container bg-white border-solid border-2 border-red-700">
-                {/* <div className="container-title">{this.props.title}</div> */}
                 <div className="container-content">{this.props.children}</div>
             </div>
         )
