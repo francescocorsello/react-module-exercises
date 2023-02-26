ad603f2d6223290eaf67a89064b46376820f80ca
Francesco Corsello <44068027+francescocorsello@users.noreply.github.com>
2023-02-26 16:18:51 +0100

    Update Welcome.js

--- react_router_01/src/Welcome.js
+++ react_router_01/src/Welcome.js
@@ -1,9 +1,6 @@
+import { useParams } from "react-router-dom"
 
-
-export function Welcome(props) {
-    return (
-        <div>
-            <h1>Hello, {props.name}! </h1>
-        </div>
-    )
+export function Welcome(){
+    const {name = "Alessandro"} = useParams()
+    return <h1>Hello {name}!</h1>
 }
