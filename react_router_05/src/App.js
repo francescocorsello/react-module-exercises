4c6046e2976aa58beaca06caff6e527417a2dc3c
Francesco Corsello <francesco.corsello@gmail.com>
2023-02-26 19:17:20 +0100

    initial commit

---
+++ react_router_05/src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes, Link } from "react-router-dom";
+import { Counter } from "./Counter";
+import { Welcome } from "./Welcome";
+import { ShowGithubUser } from "./ShowGithubUser";
+
+export function App(){
+    return(
+         <div>
+           <div>
+            <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> |{" "}
+            <Link to="users/:usernamee">Github user</Link>
+             </div>
+        <Routes>
+            <Route path="/" element={<Welcome/>}/>
+            <Route path="/:name" element={<Welcome/>}/>
+            <Route path="/counter" element={<Counter/>} />
+            <Route path="users/:username" element={<ShowGithubUser />} />
+
+            <Route path="*" element={<div><p>Not Found</p><Link to="/">Go Home</Link></div>}/>
+        </Routes>
+        </div>
+    )
+}
