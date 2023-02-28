e7c4af30221cc78fbac6307e1dd5bdc228574afd
Francesco Corsello <francesco.corsello@gmail.com>
2023-02-28 22:40:32 +0100

    initial commit

--- swr_01/src/GithubUser.js
+++ swr_01/src/GithubUser.js
@@ -8,7 +8,6 @@ export function GithubUser({ username }) {
   }
   return (
     <div>
-      <h1>CIAO</h1>
       <button onClick={handleGetUserData}>Load user data</button>
       {data && <h1>{data.name}</h1>}
     </div>
